0442f3f8515c9d7ce5d28fd1d31f997ca7ef60e2
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-24 17:52:32 -0400

    chore: update

--- src/components/ButtonCTA/ButtonCTA.tsx
+++ src/components/ButtonCTA/ButtonCTA.tsx
@@ -12,14 +12,12 @@ export const ButtonCTA: FC = () => {
   return (
     <div className="flex justify-center pt-6">
       <div className="inline-flex space-x-6">
-        <a
-          href="https://app.mosh.lol"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-flex justify-center items-center py-2 px-4 text-sm font-medium leading-6 text-center text-trueGray-50 bg-gradient-to-r from-pink-400 hover:from-pink-300 via-purple-400 hover:via-purple-300 to-indigo-500 hover:to-indigo-400 rounded-md border hover:border-2 border-gray-600 shadow-xl hover:scale-105"
+        <button
+          disabled
+          className="inline-flex justify-center items-center py-2 px-4 text-sm font-medium leading-6 text-center text-trueGray-50 disabled:hover:bg-gray-300 bg-gradient-to-r from-pink-400 hover:from-pink-300 disabled:hover:from-pink-400 via-purple-400 hover:via-purple-300 disabled:hover:via-purple-400 to-indigo-500 hover:to-indigo-400 disabled:hover:to-indigo-500 rounded-md border hover:border-2 disabled:hover:border border-gray-600 shadow-xl disabled:opacity-60 hover:scale-105 disabled:hover:scale-100"
         >
           Enter App
-        </a>
+        </button>
         <button
           className="inline-flex justify-center items-center py-2 px-4 text-sm font-medium leading-6 text-center text-pink-400 bg-pink-100 hover:bg-pink-200 rounded-md border border-transparent hover:border-red-400 focus:ring-pink-500"
           onClick={openModal}
